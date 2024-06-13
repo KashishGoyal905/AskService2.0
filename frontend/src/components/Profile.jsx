d136f308bd88dd436eb727732b9bd4f6910b26f6
Kashish Goyal <84564789+KashishGoyal905@users.noreply.github.com>
2024-06-13 15:13:01 +0530

    Implemented Cloudinary for robut file uploads

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -83,7 +83,7 @@ export default function Profile() {
                 <div className='flex flex-row flex-wrap justify-around mt-10'>
                     <div className="card w-96 glass my-6 mx-4">
                         {myUser.image
-                            ? <figure><img src={`${process.env.REACT_APP_BACKEND_URL}/uploads/images/${myUser.image}`} alt="ProfilePic" style={{ height: 300, width: 450, }} /></figure>
+                            ? <figure><img src={myUser.image} alt="ProfilePic" style={{ height: 300, width: 450, }} /></figure>
                             : <figure><img src={user1} alt="ProfilePic" style={{ height: 300, width: 450, }} /></figure>}
                         <div className="card-body">
                             <h2 className="card-title">{myUser.name}</h2>
